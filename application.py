6fd2df00b37ff7264756bba51182ec554410cb4a
adarsh0016 <adarshprakash16@gmail.com>
2021-05-12 16:44:29 +0530

    addeds the file

---
+++ application.py
@@ -0,0 +1,110 @@
+import os
+from threading import local
+from time import localtime, strftime
+from flask import Flask, render_template,url_for,redirect, flash
+from flask_login import LoginManager, login_user, current_user, login_required, logout_user
+from flask_socketio import SocketIO, send, emit, join_room, leave_room
+
+from wtform_fields import *
+
+from models import *
+
+# configure app
+app = Flask(__name__)
+app.secret_key = "scerte"
+
+#configure database
+app.config['SQLALCHEMY_DATABASE_URI']="postgresql://kjmnklnutbgmql:f09236b3dc8dd29d7d57e2975069017ae82615523f17d633f96af15ad5316ced@ec2-18-215-111-67.compute-1.amazonaws.com:5432/d69460qpq6a4hq"
+db = SQLAlchemy(app)
+
+#initialize Flask-SocketIO
+socketio = SocketIO(app)
+
+ROOMS = ["lounge", "news", "games", "coding"]
+
+# configure flask login
+login = LoginManager(app)
+login.init_app(app)
+
+@login.user_loader
+def load_user(id):
+
+    return User.query.get(int(id))
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+
+    reg_form = RegistrationForm()
+
+    # updated databse if validation success
+    if reg_form.validate_on_submit():
+        username = reg_form.username.data
+        password = reg_form.password.data
+
+        #hash password
+        hashed_pswd = pbkdf2_sha256.hash(password)
+
+        # add user into db
+        user = User(username=username, password=hashed_pswd)
+        db.session.add(user)
+        db.session.commit()
+
+        flash('Registered succesfully. Please login.', 'success')
+        return redirect(url_for('login'))
+
+    return render_template("index.html", form=reg_form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+
+    login_form = LoginForm()
+
+    # Allow login if validation success
+    if login_form.validate_on_submit():
+        user_object = User.query.filter_by(username=login_form.username.data).first()
+        login_user(user_object)
+        return redirect(url_for('chat'))
+
+        return "Not logged in!"
+
+    return render_template("login.html", form=login_form)
+
+@app.route("/chat", methods=['GET', 'POST'])
+def chat():
+    #if not current_user.is_authenticated:
+    #    flash('Please login.', 'danger')
+    #    return redirect(url_for('login'))
+
+    return render_template('chat.html', username=current_user.username, rooms=ROOMS)
+
+@app.route("/logout", methods=['GET'])
+def logout():
+    logout_user()
+    flash('You have logged out successfully', 'success')
+    return redirect(url_for('login'))
+
+#event handler (socketIO)
+@socketio.on('message')
+def message(data):
+
+    print(f"\n\n{data}\n\n")
+    send({'msg': data['msg'], 'username': data['username'], 'time_stamp': strftime('%b-%d %I: %M%p', localtime())}, room =data['room'])
+
+@socketio.on('join')
+def join(data):
+    join_room(data['room'])
+    send({'msg': data['username'] + " has joined the " + data['room'] + " room."}, room=data['room'])
+
+@socketio.on('leave')
+def leave(data):
+    leave_room(data['room'])
+    send({'msg': data['username'] + " has left the " + data['room'] + " room."}, room=data['room'])
+
+
+if __name__ == "__main__":
+
+    socketio.run(app, debug=True)
+
+
+
+    ## https://www.youtube.com/watch?v=7EeAZx78P2U
